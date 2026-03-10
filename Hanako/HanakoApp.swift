04ad72270477d4db4d1d49f9cc308bf1715316f7
Michael Placido <mpfx01@gmail.com>
2026-03-10 12:57:04 +0900

    Initial Commit

---
+++ Hanako/HanakoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanakoApp.swift
+//  Hanako
+//
+//  Created by Michael Placido on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct HanakoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
